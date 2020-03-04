8b0503ed754b99099cec3d8b7b957f675c3165c4
unknown <oldjun@sina.com>
2020-03-04 17:11:05 +0800

    a tiny c++ inifile parser

--- main.cpp
+++ main.cpp
@@ -1,27 +1,13 @@
-# yazi-ini
-a tiny c++ inifile parser，provides a C++ interface for parsing (reading) and dumping (writing) .ini files.
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <stdio.h>
+#include <time.h>
+#include <errno.h>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
-|Author|junping|
-|---|---|
-|E-mail|oldjun@sina.com|
-|Wechat|chenjunping1024|
-
-## Definition of Initialization file
-```ini
-[server]
-ip = 127.0.0.1
-port = 80
-
-[profile]
-name = junping
-phone = 189****5599
-sex = man
-age = 30
-height = 1.70
-```
-
-## Load & dump a ini file
-```c++
 #include "IniFile.h"
 using namespace yazi::utility;
 
@@ -41,22 +27,6 @@ int main()
 
     // dumping the ini file to another file
     ini.save("./main2.ini");
-    
-    return 0;
-}
-```
-
-## Get & Set configuration option
-
-```c++
-#include "IniFile.h"
-using namespace yazi::utility;
-
-int main()
-{
-    // loading a ini file by construct function
-    const string & filename = "./main.ini";
-    IniFile ini(filename);
 
     // get config option by array access
     const string & name = ini["profile"]["name"];
@@ -74,23 +44,7 @@ int main()
 
     age = ini["profile"]["age"];
     std::cout << "in section profile change age to " << age << std::endl;
-    
-    return 0;
-}
-```
 
-## Modify section & key
-
-```c++
-#include "IniFile.h"
-using namespace yazi::utility;
-
-int main()
-{
-    // loading a ini file by construct function
-    const string & filename = "./main.ini";
-    IniFile ini(filename);
-    
     // if the server section is exist
     if (ini.has("server"))
     {
@@ -108,20 +62,9 @@ int main()
 
     // remove the sever section
     ini.remove("server");
-    
+
     // clear the ini file
     ini.clear();
-    
+
     return 0;
 }
-```
-
-## Compile & Run the project
-download the source code, cd into the yazi-ini project working directory, run command make && ./main 
-```bash
-cd ./yazi-ini
-make
-./main
-```
-## Testing
-file main.ini used to test the ini file parse function. 
